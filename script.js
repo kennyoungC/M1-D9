73425dfb2022d1b796ab3fb948a56e59ea87ccbb
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-27 23:36:04 +0200

    creating boxes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const bingoBox = document.querySelector(`.bingo-box`);
+console.log(bingoBox);
+const toDisplayCells = function () {
+  for (cellNumber = 1; cellNumber <= 76; cellNumber++) {
+    let newBingoBox = document.createComment(`div`);
+    newBingoBox.innerHTML = cellNumber;
+    newBingoBox.id = bingoBox;
+    bingoBox.appendChild(newBingoBox);
+  }
+};
+console.log(toDisplayCells());
