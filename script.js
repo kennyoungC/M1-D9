f55fb95cadb893e41ed7a5ccd22001beed88c771
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-28 14:12:07 +0200

    exercise 9 solved

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const bingoBox = document.querySelector(`.bingo-box`);
-console.log(bingoBox);
 const toDisplayCells = function () {
   for (let cellNumber = 1; cellNumber <= 76; cellNumber++) {
     let newBingoBox = document.createElement(`div`);
     newBingoBox.innerHTML = cellNumber;
     newBingoBox.classList.add(`cell`);
-    // newBingoBox.id = bingoBox;
+    newBingoBox.id = `cell${cellNumber}`;
     bingoBox.appendChild(newBingoBox);
   }
+  return bingoBox;
 };
+
 toDisplayCells();
-const highl
+
+const toBeHighlighted = function () {
+  let random = Math.trunc(Math.random() * 76 + 1);
+  let cell = document.querySelector(`#cell${random}`);
+  cell.classList.add(`highlighed-cell`);
+  const randomText = document.querySelector(`.randomText`);
+  randomText.textContent = `Random Number: ${random}`;
+};
